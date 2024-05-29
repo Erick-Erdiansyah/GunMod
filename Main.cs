33060ad93a495bb2ccee796b5a3546db14d8fde4
Erick-Erdiansyah <erdiansyah290@gmail.com>
2024-05-29 15:49:35 +0700

    initial commit

---
+++ Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using main.Patches;
+
+namespace main
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        private const string modGUID = "Librarian.Test";
+        private const string modName = "GunMod";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static Main instance;
+
+        internal ManualLogSource logSource;
+
+        void Awake()
+        {
+            if (instance == null) 
+            { 
+                instance = this;
+            }
+
+            logSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            logSource.LogInfo("hello, I'll add something here later");
+
+            harmony.PatchAll(typeof(Main));
+            harmony.PatchAll(typeof(UnlimitedArmor));
+
+        }
+    }
+}
---
+++ Patches/UnlimitedArmor.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+
+namespace main.Patches
+{
+    [HarmonyPatch(typeof(HealthHaver),MethodType.Getter)]
+    internal class UnlimitedArmor
+    {
+        [HarmonyPatch(nameof(HealthHaver.Armor))]
+        [HarmonyPostfix]
+        static void Invincible(ref float __result) 
+        {
+            try
+            {
+                __result = 20;
+            }
+            catch (Exception e) {
+                var log = new ManualLogSource("logger");
+                BepInEx.Logging.Logger.Sources.Add(log);
+                log.LogInfo(e.ToString());
+                BepInEx.Logging.Logger.Sources.Remove(log);
+            }
+        }
+
+    }
+
+}
