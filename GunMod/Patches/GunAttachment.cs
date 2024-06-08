2960da268fb08b1ea665b578f518badd2a4d9672
Erick-Erdiansyah <erdiansyah290@gmail.com>
2024-06-08 16:05:18 +0700

    add map

--- GunMod/Patches/GunAttachment.cs
+++ GunMod/Patches/GunAttachment.cs
@@ -18,8 +18,6 @@ namespace GunMod.Patches
             try
             {
                 __result = 100;
-                __instance.ammo = 100;
-                __instance.blankDamageToEnemies = 100;
                 __instance.InfiniteAmmo = true;
                 __instance.reloadTime = 0;
                 __instance.CriticalChance = 2;
@@ -43,11 +41,13 @@ namespace GunMod.Patches
         {
             try
             {
-                __result = 3;
+                __result = 0;
                 __instance.healthHaver.Armor = 10;
                 __instance.startingGunIds.Clear();
-                // the strale gun
-                __instance.startingGunIds.Add(542);
+                // the strale gun = 542, yariLauncer = 16, cannon = 480 , dragunfire = 670 
+                __instance.startingGunIds.Add(542); // stratle
+                __instance.startingGunIds.Add(480); // cannon
+                __instance.startingPassiveItemIds.Add(137); // map
             }
             catch (Exception e)
             {
